fa47f2d7c1a8e6b236eb28c320cfc9641b3936db
leomet07 <leomet07@gmail.com>
2025-05-20 11:00:46 -0400

    simulate spinning wheel of 1kg weight

--- script.py
+++ script.py
@@ -8,34 +8,48 @@ coeff_friction = 0.2
 xvel_graph = graph(width=350, height=250, xtitle=("Time"), ytitle=("Velocity"), align='left')
 xvel_graph_dots =gdots(color=color.red, graph=xvel_graph)
 
-class MyBox():
-    def __init__(self, vel, mass):
-        self.vel = vel
+class MyWheel():
+    def __init__(self, a_vel, mass, radius):
+        self.a_vel = a_vel
         self.mass = mass
-        self.box = box(pos=vec(0, 0, 0), length=3, height=2, width=1, color=color.cyan) # width is irrelavant
+        self.radius = radius
+        self.inertia = self.mass * (self.radius**2)
+        
+        self.sphere = sphere(pos=vec(0, 0, 0), radius=self.radius, color=color.cyan)
     
-    def calculate_acc(self, net_force):
-        return net_force * (1.0/self.mass)
+    def calculate_a_acc(self, net_torque): # in z land bc rotatioin of wheel along x
+        return net_torque * (1.0/self.inertia)
+    
+    def calculate_tangential_velocity(self):
+        return vec(self.a_vel.mag * self.radius, 0, 0)# convert z axis rotations to x axis movement
     
     def update(self):
-        self.box.pos = self.box.pos + self.vel*dt
+        self.sphere.pos = self.sphere.pos + self.calculate_tangential_velocity()*dt
         
+        braking_force_mag = 20 # in newtons, this is an arbitrary braking force, function of hand squeeze
+        friction_torque = (-1) * (braking_force_mag) * hat(self.a_vel) # -1 to oppose, hat of a zero vector is just zero! therefore friction stops when a_vel=0
+        net_torque = friction_torque
         
-        friction_vetor = (-self.mass*g * coeff_friction) * hat(self.vel) # hat of a zero vector is just zero! therefore friction stops when vel=0
-        net_force = friction_vetor
+        a_acc = self.calculate_a_acc(net_torque)
         
-        acc = self.calculate_acc(net_force)
-        
-        self.vel = self.vel + acc*dt
+        self.a_vel = self.a_vel + a_acc*dt
 
-mybox = MyBox(vec(10, 0,0), 60)
-scene.camera.pos=vector(2, 5, 25) # This tells VPython to view the scene from the position (0,5,10)
+wheel_diameter = 622.0 / 1000.0#(mm) /1000
+wheel_radius = wheel_diameter * 0.5
+wheel_weight = 1 # kg
+
+bike_speed_kmh = 25 # km/h
+bike_speed = bike_speed_kmh * (1000 / 3600)
+bike_rpm = bike_speed / (2.0 * pi * wheel_radius)
+
+mywheel = MyWheel(vec(0, 0, -4), wheel_weight, wheel_radius)
+scene.camera.pos=vector(2, 5, 15) # This tells VPython to view the scene from the position (0,5,10)
 
 while True:
     rate(1/dt)
     
-    mybox.update()
+    mywheel.update()
     
-    xvel_graph_dots.plot(t, mybox.vel.x)
+    xvel_graph_dots.plot(t, mywheel.calculate_tangential_velocity().x)
     
     t += dt
